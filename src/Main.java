4618cd9d9b4ec262a6a25ca77b4fb3726324f7fe
Tim Sakhaulov <timur.sakhaulov@gmail.com>
2022-03-21 18:20:53 +0500

    added comments

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
+        //Созадали коробки с фруктами
         Box<Orange> orangeBox = new Box<>();
         Box<Apple> appleBox = new Box<>();
         Box<Orange> orangeBoxNew = new Box<>();
         Box<Orange> orangeBoxAnother = new Box<>();
 
+        //Создали объекты фруктов
         Orange orange = new Orange();
         Apple apple = new Apple();
 
+        //Заполняем коробки фруктами
         Orange[] tempOrange = {orange, orange};
         Apple[] tempApple = {apple, apple, apple};
 
@@ -24,6 +27,7 @@ public class Main {
             orangeBoxAnother.addContent(value);
         }
 
+        //Проверяем работу методов Box
         System.out.println("Вес первой коробки: " + orangeBox.getWeight() + "\n" +
                            "Вес второй коробки: " + appleBox.getWeight() + "\n" +
                            "Сравнили их вес, результат: " + orangeBox.compareWeight(appleBox) + "\n");
@@ -34,13 +38,11 @@ public class Main {
                            "Вес третьей коробки: " + orangeBoxNew.getWeight() + "\n" +
                            "Сравнили их вес, результат: " + orangeBox.compareWeight(orangeBoxNew) + "\n");
 
-
         System.out.println("Переместили содержимое третьей коробки в четвертую коробку с весом " + orangeBoxAnother.getWeight());
         orangeBoxNew.moveContent(orangeBoxAnother);
         System.out.println("Вес третьей коробки: " + orangeBoxNew.getWeight() + "\n" +
                            "Вес четвёртой коробки: " + orangeBoxAnother.getWeight() + "\n" +
                            "Сравнили их вес, результат: " + orangeBoxNew.compareWeight(orangeBoxAnother) + "\n");
-
 
     }
 }
