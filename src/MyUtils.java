8a64a496f8410d5a7abeb31d29e1e8fc10ab1b8c
Tim Sakhaulov <timur.sakhaulov@gmail.com>
2022-03-21 20:30:54 +0500

    Added swapArrayElems method

--- src/MyUtils.java
+++ src/MyUtils.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class MyUtils<Type> {
     public void swapArrayElems(ArrayList<Type> ar, int indexFirst, int indexSecond) {
         try {
-            if (ar.size() >= 2) {
+            if (ar.size() >= 2 && indexFirst != indexSecond) {
                 Type temp = ar.get(indexSecond);
                 ar.set(indexSecond, ar.get(indexFirst));
                 ar.set(indexFirst, temp);
